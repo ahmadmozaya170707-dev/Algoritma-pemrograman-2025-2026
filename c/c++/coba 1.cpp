8cd844edbc234f81351cca59bbe988b59a3a5c61
ahmadmozayaputra <ahmadmozayaputra170707@gmail.com>
2025-10-13 09:42:37 +0700

    nothing

---
+++ c/c++/coba 1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    int a=7;
+    int b=5;
+    char c[17]="ahmad mozaya putra";
+
+    puts("nama");
+    puts(c);
+    printf("teridri dari%dhurup dan %Dkata , a,b");
+
+}
