f6c2d1cbdac705b77391336a1415b45508cb4084
ahmadmozayaputra <ahmadmozayaputra170707@gmail.com>
2025-10-13 10:36:46 +0700

    latihan 1

---
+++ c/c++/latihan 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string nama="putra";
+    int umur=17;
+    float tinggi=155;
+    bool aktif=true;
+
+    cout<<"nama:" <<nama<<endl;
+    cout<<"umur:" <<umur<<endl;
+    cout<<"tinggi:" <<tinggi<<endl;
+    cout<<"status aktif:" <<aktif<<endl;
+    return0;
+}
