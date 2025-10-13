07b84f151f1a794a72f7954dab8dcf46b669fc85
ahmadmozayaputra <ahmadmozayaputra170707@gmail.com>
2025-10-13 10:58:07 +0700

    java

--- java/latihan 1.java
+++ java/latihan 1.java
@@ -1,5 +1,5 @@
 public class Main{
-    public static void main(String[]args)
+    public static void main(String[]args){
         string nama="putra";
         int umur=17;
         double tinggi=155;
