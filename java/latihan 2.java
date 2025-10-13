9b24387e38678f3fb85018c4fdc71c4c8cfdd5d8
ahmadmozayaputra <ahmadmozayaputra170707@gmail.com>
2025-10-13 16:49:58 +0700

    latihan c++ 2

---
+++ java/latihan 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class latihan 2 {
+    public static void main(string[]args){
+        final doublePI=3.14159;
+        scanner sc=new scanner(system.in);
+        system.out.print("masukan jari jari:");
+        double r=sc.nextdouble();
+        double lias=PI*r*r;
+        system.out.println("luas lingkaran="+luas);
+    }
+}
