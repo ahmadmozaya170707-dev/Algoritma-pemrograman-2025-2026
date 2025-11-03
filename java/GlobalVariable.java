655f28a5837bef82e05d9887407e642911efa19b
ahmadmozayaputra <ahmadmozayaputra170707@gmail.com>
2025-11-03 19:46:23 +0700

    tugas

---
+++ java/GlobalVariable.java
@@ -0,0 +1,7 @@
+public class GlobalVariable {
+public static int age; // global variable
+public static void main(String args[]) {
+age = 36;
+System.out.println("ahmad mozaya putra : " + age);
+}
+}
---
+++ php/konstanta1.php
@@ -0,0 +1,7 @@
+<?php
+define("NAMA", "ahmad mozaya putra");
+define("NILAI", 90);
+echo "---Belajar Konstanta --- <br><br>";
+echo "Nama : " . NAMA;
+echo "<br>Nilai : " . NILAI;
+?>
