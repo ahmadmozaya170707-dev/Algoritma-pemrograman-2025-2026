f329b0be38616ce3a3ab9737346498004f7fc163
ahmadmozayaputra <ahmadmozayaputra170707@gmail.com>
2026-01-19 10:13:47 +0700

    uas

--- UAS/UAS.java
+++ UAS/UAS.java
@@ -1,14 +1,17 @@
 public class UAS {
-
     public static void main(String[] args) {
         int n = 5;
-        int i, j;
+        int i, j, k;
 
-        for (i = 0; i < n; i++) {       
-            for (j = 0; j <= i; j++) { 
+        for (i = 0; i < n; i++) {
+            for (k = n - 1; k > i; k--) {
+                System.out.print(" ");
+            }
+            for (j = 0; j <= i; j++) {
                 System.out.print(j);
-         }
-            System.out.println();       
+            }
+            
+            System.out.println();
         }
     }
 }
