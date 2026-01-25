aa9172c8657b7a29508f5bf4c6373df1dadea189
ahmadmozayaputra <ahmadmozayaputra170707@gmail.com>
2026-01-25 19:10:18 +0700

    moza

---
+++ java/exampleforeach.java
@@ -0,0 +1,9 @@
+public class exampleforeach { 
+public static void main(String[] args) { 
+// TODO Auto-generated method stub 
+String[] cars = {"red", "green", "blue", "yellow"}; 
+for (String i : cars) { 
+System.out.println(i); 
+} 
+} 
+} 
